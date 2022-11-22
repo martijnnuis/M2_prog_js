706bf1de8b68afa3aed8521111d5f2a8c9c45a18
martijn <35205@ma-web.nl>
2022-11-22 12:48:55 +0100

    psuh 1

---
+++ Les_01/app.js
@@ -0,0 +1,23 @@
+class App
+{
+    runApplication()
+    {
+        let appnaam = "discord";
+        let versienummer = 0.9;
+        let versiedatum = 22-11-2022;
+        let autheur = "Martijn Nuis";
+        let copyright = "Martijn Nuis";
+        let distributeur = "henk Jan";
+        let darkmode = false;
+        console.log(appnaam);
+        console.log(versienummer);
+        console.log(versiedatum);
+        console.log(autheur);
+        console.log(copyright);
+        console.log(distributeur);
+        console.log(darkmode);
+    }
+
+}
+let app = new App();
+app.runApplication();
